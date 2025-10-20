eb2302b4f6e26ee9c08323bb53da84415e9c9392
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-20 18:12:24 +0100

    feat: add ListGitLabProjects function

--- core/repositories/gitlab_handler.go
+++ core/repositories/gitlab_handler.go
@@ -1,1 +1,38 @@
 package repositories
+
+import (
+	"log"
+
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+func ListGitLabProjects(client *gitlab.Client) ([]*gitlab.Project, error) {
+	// Pagination
+	options := &gitlab.ListProjectsOptions{
+		Membership: gitlab.Ptr(true),
+		ListOptions: gitlab.ListOptions{
+			Page:    1,
+			PerPage: 50,
+		},
+	}
+
+	var allProjects []*gitlab.Project
+
+	for {
+		projects, response, err := client.Projects.ListProjects(options)
+		if err != nil {
+			return nil, err
+		}
+
+		allProjects = append(allProjects, projects...)
+
+		if response.CurrentPage >= response.TotalPages {
+			break
+		}
+
+		options.Page = response.NextPage
+	}
+
+	log.Printf("Fetched %d projects from GitLab", len(allProjects))
+	return allProjects, nil
+}
