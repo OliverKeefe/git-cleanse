41daad64fd1e92f786c73401f02fdb5b79808881
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-20 18:12:03 +0100

    feat: add GetGitLabClient function

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -1,5 +1,14 @@
 package auth
 
+import (
+	"context"
+	"log"
+
+	"github.com/google/go-github/v61/github"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+	"golang.org/x/oauth2"
+)
+
 type Accounts struct {
 	Github    []string
 	GitLab    []string
@@ -12,4 +21,15 @@ func GetGitHubClient(token string) *github.Client {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(ctx, ts)
 	return github.NewClient(tc)
+}
+
+func GetGitLabClient(token string) (*gitlab.Client, error) {
+	git, err := gitlab.NewClient(token, gitlab.WithBaseURL("https://cseegit.essex.ac.uk/"))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	} else {
+		log.Println("Success!")
+	}
+
+	return git, err
 }
