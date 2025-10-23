d3316a0a8212359d5149eaf848b9f524bfef2d8d
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-23 15:58:34 +0100

    feat: add navigation.go

--- cmd/gitcleanse/main.go
+++ cmd/gitcleanse/main.go
@@ -15,8 +15,30 @@ type model struct {
 }
 
 func main() {
-	p := tea.NewProgram(model{})
-	if err := p.Start(); err != nil {
+	//client, err := auth.GetGitLabClient("glpat-AMtIP5ZQi-mGhJKD7TSfEW86MQp1OjcwaAk.01.0z08wjz3y")
+	//if err != nil {
+	//	log.Fatalf("%s", err)
+	//}
+	//
+	//version, _, err := client.Version.GetVersion()
+	//
+	//if err != nil {
+	//	log.Printf("Warning: could not fetch GitLab version: %v", err)
+	//} else {
+	//	log.Printf("Connected to GitLab API version: %s", version.Version)
+	//}
+
+	//projects, err := repositories.ListGitLabProjects(client)
+	//users, err := repositories.ListGitLabUsers(client)
+
+	//for _, u := range users {
+	//	log.Printf("%s", u)
+	//}
+
+	//for _, p := range projects {
+	//	log.Printf("%s", p.NameWithNamespace)
+	//}
+
 	program := tea.NewProgram(routes.NewRootModel())
 	if _, err := program.Run(); err != nil {
 		fmt.Println("Error:", err)
