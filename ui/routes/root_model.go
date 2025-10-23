b9a317cdd1728de17151e3a644b57b3b457d32e2
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-23 13:57:25 +0100

    feat: add page routing logic

--- ui/routes/root_model.go
+++ ui/routes/root_model.go
@@ -1,1 +1,13 @@
 package routes
+
+import tea "github.com/charmbracelet/bubbletea"
+
+type RootModel struct {
+	currentPage PageID
+	pages       map[PageID]tea.Model
+}
+type PageID string
+
+const (
+	StartMenu PageID = "startMenu"
+)
