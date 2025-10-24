e0d0b6b00eb29aac823bef78f52fc6a826244d0e
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-24 17:40:21 +0100

    refactor: move types to dedicated pkg

---
+++ core/types/types.go
@@ -0,0 +1,17 @@
+package types
+
+type PageID string
+type NavigateMsg struct {
+	To PageID
+}
+type AuthSubmittedMsg struct {
+	Platform string
+	Email    string
+	Token    string
+}
+
+type Commit struct {
+	Header string
+	Body   string
+	Footer string
+}
