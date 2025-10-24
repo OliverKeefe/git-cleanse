e2d39892f0c78d589eca7928263269e647e6b0ef
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-24 15:37:41 +0100

    feat: add rewriter struct and constructor

--- core/rewriter.go
+++ core/rewriter.go
@@ -1,1 +1,16 @@
 package core
+
+type ToReplace []string
+type DontReplace []string
+
+type Rewriter struct {
+	PII    ToReplace
+	notPII DontReplace
+}
+
+func NewRewriter(replace []string, dontReplace []string) Rewriter {
+	return Rewriter{
+		PII:    replace,
+		notPII: dontReplace,
+	}
+}
