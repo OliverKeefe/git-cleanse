77d6f57bf119a1e72a7acd7be231d1c54be4363b
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-28 20:49:00 +0000

    feat: update scanner.go

--- core/scanner.go
+++ core/scanner.go
@@ -1,1 +1,26 @@
 package core
+
+import (
+	"fmt"
+
+	git "github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
+)
+
+type ScanFor []string
+
+// Open repo func
+
+//// Search repo func - call rewrite
+//
+//func (scan ScanFor) ScanRepo(repoPath string) (bool, error) {
+//	repo, err := git.PlainOpen(repoPath)
+//	if err != nil {
+//		return fmt.Errorf("failed to open repo: %w", err)
+//	}
+//
+//	worktree, err := repo.Worktree()
+//	if err != nil {
+//		return err
+//	}
+//}
