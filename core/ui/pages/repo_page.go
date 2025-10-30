ff9e840372db5729573f921d79fdf37f8f1fc7eb
OliverKeefe <oliverkeefe@tutanota.com>
2025-10-30 19:11:10 +0000

    feat: add RepoPageModel struct

--- core/ui/pages/repo_page.go
+++ core/ui/pages/repo_page.go
@@ -1,6 +1,36 @@
 package pages
 
+import (
+	"github.com/OliverKeefe/git-cleanse/core"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
 type RepoItem struct {
 	title       string
 	description string
+}
+
+type RepoPageModel struct {
+	cursor       int
+	repositories []RepoItem
+	selected     string
+	width        int
+	height       int
+}
+
+func NewRepoPageModel() RepoPageModel {
+	return RepoPageModel{
+		cursor:       0,
+		repositories: core.GetRepo(),
+	}
+}
+
+func (model RepoPageModel) Init() tea.Cmd { return nil }
+
+func (model RepoPageModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+}
+
+func (model RepoPageModel) View() string {
+
 }
