11e89d8bba6eb0e77e28705ab36bfbdb8a0a469c
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-11-03 15:27:49 +0000

    feat: add NewRepoPageModel constructor method

--- core/ui/pages/repo_page.go
+++ core/ui/pages/repo_page.go
@@ -1,36 +1,69 @@
 package pages
 
 import (
+	"fmt"
+	_ "github.com/OliverKeefe/git-cleanse/core/repositories"
 	"github.com/OliverKeefe/git-cleanse/core"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/go-git/go-git/v6"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
 
 type RepoItem struct {
-	title       string
-	description string
+	Title       string
+	Description string
 }
 
 type RepoPageModel struct {
-	cursor       int
-	repositories []RepoItem
-	selected     string
-	width        int
-	height       int
+	Cursor             int
+	ListOfRepositories []RepoItem
+	Repository         []*git.Repository
+	Selected           string
+	Width              int
+	Height             int
 }
 
-func NewRepoPageModel() RepoPageModel {
-	return RepoPageModel{
-		cursor:       0,
-		repositories: core.GetRepo(),
+func NewRepoPageModel(path string, uri string, provider string) (RepoPageModel, error) {
+	var repoList []RepoItem
+
+	switch provider {
+	case:
+		"GitLab"
+		{
+			client, err := gitlab.NewClient(token)
+			if err != nil {
+				return RepoPageModel{}, fmt.Errorf("failed to get GitLab client %e", err)
+			}
+
+			projects, _, err := client.Projects.ListProjects(&ListProjectsOptions{
+				Membership: gitlab.Bool(true),
+				Simple:     gitlab.Bool(true),
+			})
+			if err != nil {
+				return RepoPageModel{}, fmt.Errorf("failed to get GitLab projects %e", err)
+			}
+
+			for _, r := range projects {
+				repoList = append(repoList, RepoItem{
+					Title:       r.Name,
+					Description: r.Description,
+				})
+			}
+		}
+		return RepoPageModel{
+			Cursor:             0,
+			ListOfRepositories: repoList,
+			Repository:         r,
+		}, nil
 	}
 }
 
 func (model RepoPageModel) Init() tea.Cmd { return nil }
 
 func (model RepoPageModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
+	panic("Not implemented")
 }
 
 func (model RepoPageModel) View() string {
-
+	panic("Not implemented")
 }
