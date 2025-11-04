1caa958328244bf4e7199537444b4b02192f893d
OliverKeefe <oliverkeefe@tutanota.com>
2025-11-04 17:25:29 +0000

    feat: add NewGitLabPageModel constructor method

--- core/ui/pages/gitlab_page.go
+++ core/ui/pages/gitlab_page.go
@@ -1,8 +1,50 @@
 package pages
 
-import "github.com/go-git/go-git/v6"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/go-git/go-git/v6"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
 
 type RepoItem struct {
 	Title       string
 	Description string
+}
+
+type GitLabPageModel struct {
+	Cursor             int
+	ListOfRepositories []RepoItem
+	Repository         []*git.Repository
+	Selected           string
+	Width              int
+	Height             int
+}
+
+func NewGitLabPageModel(token string, uri string, baseUrl string) (GitLabPageModel, error) {
+	var projectList []RepoItem
+
+	client, err := gitlab.NewClient(token)
+	if err != nil {
+		return GitLabPageModel{}, fmt.Errorf("unable to get gitlab client %e", err)
+	}
+
+	projects, _, err := client.Projects.ListProjects(&gitlab.ListProjectsOptions{
+		Membership: gitlab.BoolValue(true),
+		Simple:     gitlab.BoolValue(true),
+	})
+	if err != nil {
+		return RepoPageModel{}, fmt.Errorf("failed to get gitlab projects %e", err)
+	}
+
+	for _, p := range projects {
+		projectList = append(projectList, RepoItem{
+			Title:       p.Name,
+			Description: p.Description,
+		})
+	}
+	return GitLabPageModel{
+		Cursor:             0,
+		ListOfRepositories: projectList,
+	}, nil
 }
